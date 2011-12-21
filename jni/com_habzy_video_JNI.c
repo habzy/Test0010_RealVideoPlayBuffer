1fe732941e9bb5ce8defe37303819e2df893b102
habzy <habzyhs@gmail.com>
2011-12-21 15:41:25 +0800

    build a so .. test is the jni worked.

--- jni/com_habzy_video_JNI.c
+++ jni/com_habzy_video_JNI.c
@@ -5,10 +5,9 @@
  * Method:    adjustFD
  * Signature: (Ljava/io/FileDescriptor;)Z
  */
-JNIEXPORT jboolean JNICALL Java_com_habzy_video_JNI_adjustFD(JNIEnv *, jobject,
-        jobject)
+JNIEXPORT jboolean JNICALL Java_com_habzy_video_JNI_adjustFD(JNIEnv * env,
+        jobject thiz, jobject fd)
 {
-
 
 }
 
